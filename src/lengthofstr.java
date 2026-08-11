b9604b4da3952421e6fed093f546813b27bcf07d
adityakumar <kumaaraditya11@gmail.com>
2026-08-11 10:30:51 +0530

    commit

---
+++ src/lengthofstr.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class lengthofstr {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println("string length: "+str.length());
+    }
+}
