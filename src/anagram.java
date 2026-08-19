5ffd444c49f6d67377affd59a99d0ddc02c83f4b
adityakumar <kumaaraditya11@gmail.com>
2026-08-19 19:23:46 +0530

    commit

---
+++ src/anagram.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class anagram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+
+        System.out.println(isAnagram(s1, s2));
+    }
+
+    public static boolean isAnagram(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+}
